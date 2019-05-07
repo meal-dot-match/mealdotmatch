1708f85f5c5dfffac96bedd55c9dde8b43a53670
Katie Sundstrom <ksiegel@wellesley.edu>
2019-05-07 12:17:09 -0400

    adds sign in functionality to button

--- client/components/home.js
+++ client/components/home.js
@@ -1,15 +1,25 @@
 import React from 'react'
+import {Link} from 'react-router-dom'
 
 export default class Home extends React.Component {
   constructor(props) {
     super(props)
+    this.handleClick = this.handleClick.bind(this)
   }
+
+  handleClick() {}
 
   render() {
     return (
       <div>
-        <button>Sign In</button>
+        <Link to="/login">
+          <button type="button">Sign In</button>
+        </Link>
+
+        <label>Been here before?</label>
+        <br />
         <button>Match me!</button>
+        <label>New Users</label>
       </div>
     )
   }
