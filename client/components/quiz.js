ea63cfaabfdd07f62f1fdf616b2dd84ed8ab62ea
Katie Sundstrom <ksiegel@wellesley.edu>
2019-05-07 13:25:16 -0400

    adds CuttingBoard component

--- client/components/quiz.js
+++ client/components/quiz.js
@@ -1,5 +1,6 @@
 import React from 'react'
 import {Link} from 'react-router-dom'
+import {CuttingBoard} from './index'
 
 export default class Quiz extends React.Component {
   constructor(props) {
@@ -12,14 +13,19 @@ export default class Quiz extends React.Component {
   render() {
     return (
       <div>
-        <h3>What meal would you like to make?</h3>
-        <button type="button">Breakfast</button>
-        <button type="button">Lunch</button>
-        <button type="button">Dinner</button>
-        <button type="button">Dessert</button>
-        <br />
-        <button>Previous</button>
-        <button>Next</button>
+        <div>
+          <CuttingBoard />
+        </div>
+        <div>
+          <h3>What meal would you like to make?</h3>
+          <button type="button">Breakfast</button>
+          <button type="button">Lunch</button>
+          <button type="button">Dinner</button>
+          <button type="button">Dessert</button>
+          <br />
+          <button>Previous</button>
+          <button>Next</button>
+        </div>
       </div>
     )
   }
@@ -29,4 +35,4 @@ export default class Quiz extends React.Component {
 
 // const mapDispatchToProps
 
-// export default connect(mapStateToProps, mapDispatchToProps)(Home)
+// export default connect(mapStateToProps, mapDispatchToProps)(Quiz)
