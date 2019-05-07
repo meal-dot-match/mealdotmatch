680370996efaa44256641ad0cfa63a9dacd51b2a
Katie Sundstrom <ksiegel@wellesley.edu>
2019-05-07 14:16:55 -0400

    sets up example quiz questions

--- client/components/quizQuestions.js
+++ client/components/quizQuestions.js
@@ -6,19 +6,18 @@ export default class Quiz extends React.Component {
   }
 
   render() {
-    return <div />
-    // based on something, return different sets of quiz questions :)
+    return (
+      <div>
+        <h3>What meal would you like to make?</h3>
+        <button type="button">Breakfast</button>
+        <button type="button">Lunch</button>
+        <button type="button">Dinner</button>
+        <button type="button">Dessert</button>
+        <br />
+      </div>
+      // render different quiz questions based on what page of quiz you are on. Each time a selection is made by visitor, pass that info to redux so that redux will re-render the CuttingBoard component.
+    )
   }
-}
-
-{
-  /* e.g., for page 1,
-   <h3>What meal would you like to make?</h3>
-<button type="button">Breakfast</button>
-<button type="button">Lunch</button>
-<button type="button">Dinner</button>
-<button type="button">Dessert</button>
-<br /> */
 }
 
 // const mapStateToProps
