7a1131051011fd3b7649034eb45133111dfd4994
Amy Liao <a.liao.uey@gmail.com>
2019-05-08 00:17:18 -0400

    Added a test for the about component

--- client/components/About.js
+++ client/components/About.js
@@ -1,9 +1,10 @@
 import React from 'react'
+import {connect} from 'react-redux'
 
 const About = () => {
   return (
     <div>
-      <h1> Our Story </h1>
+      <h1>Our Story</h1>
       <p>
         We want to create a recipe site that is personal to you and what you
         have in your pantry.
