fedbee72be6f62b4de558b128f44284eaa41a9ac
Ann Kettles <ann.a.kettles@gmail.com>
2019-05-10 15:12:43 -0400

    single page receiving information from results page

--- client/components/singleRecipe.js
+++ client/components/singleRecipe.js
@@ -2,6 +2,7 @@ import React from 'react'
 
 class SingleRecipe extends React.Component {
   render() {
+    console.log('WHAT ARE MY PROPS?????', this.props.location)
     return <div>Hello, this will be the recipe</div>
   }
 }
