eb5f9e98656c37b2c161f35e8f0bd2bd431768ae
Katie Sundstrom <ksiegel@wellesley.edu>
2019-05-15 00:13:00 -0400

    removes photos

--- client/components/guidedTour.js
+++ client/components/guidedTour.js
@@ -47,11 +47,6 @@ class GuidedTour extends React.Component {
           </Modal.Header>
           <Carousel>
             <Carousel.Item className="carousel-item">
-              {/* <img
-                className="d-block w-100"
-                src="https://images.unsplash.com/photo-1466978913421-dad2ebd01d17?ixlib=rb-1.2.1&auto=format&fit=crop&w=1267&q=80"
-                alt="Like cooking"
-              /> */}
               <Carousel.Caption className="about">
                 <h3>
                   {' '}
@@ -62,11 +57,6 @@ class GuidedTour extends React.Component {
               </Carousel.Caption>
             </Carousel.Item>
             <Carousel.Item className="carousel-item">
-              <img
-                className="d-block w-100"
-                src="https://images.unsplash.com/photo-1545601445-4d6a0a0565f0?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=1267&q=80"
-                alt="Stocked Pantry"
-              />
               <Carousel.Caption className="about">
                 <h3>
                   Click on ingredients in the quiz to add them to your
@@ -78,31 +68,16 @@ class GuidedTour extends React.Component {
               </Carousel.Caption>
             </Carousel.Item>
             <Carousel.Item className="carousel-item">
-              <img
-                className="d-block w-100"
-                src="https://images.unsplash.com/photo-1547860664-b8537ca5f833?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=1350&q=80"
-                alt="Women engineers"
-              />
               <Carousel.Caption className="about">
                 <h3>
                   {' '}
                   Any time you're ready to be matched with recipes, click the
                   "Match Me" button.
                 </h3>
-                <p>
-                  Sometimes we just can't make it to the store and want to make
-                  meals with the ingredients we already have. We couldn't find a
-                  resource that offered us recipes catered towards our pantries,
-                  so we built one!
-                </p>
+                <p>More text?</p>
               </Carousel.Caption>
             </Carousel.Item>
             <Carousel.Item className="carousel-item">
-              <img
-                className="d-block w-100"
-                src="https://images.unsplash.com/photo-1547860664-b8537ca5f833?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=1350&q=80"
-                alt="Women engineers"
-              />
               <Carousel.Caption className="about">
                 <h3>
                   Want to learn more? Visit our How It Works page! Ready to take
@@ -113,10 +88,20 @@ class GuidedTour extends React.Component {
             </Carousel.Item>
           </Carousel>
           <Modal.Footer>
-            <Button variant="secondary" onClick={this.handleClose}>
+            <Button
+              className="btn-responsive"
+              size="md"
+              variant="secondary"
+              onClick={this.handleClose}
+            >
               How It Works
             </Button>
-            <Button variant="primary" onClick={this.handleClose}>
+            <Button
+              className="btn-responsive"
+              size="md"
+              variant="primary"
+              onClick={this.handleClose}
+            >
               Take the Quiz
             </Button>
           </Modal.Footer>
