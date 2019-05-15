9c807bf3b801a561d6eed16a00f98406026f12e4
Katie Sundstrom <ksiegel@wellesley.edu>
2019-05-15 00:05:31 -0400

    beginning content for the guided tour modal

--- client/components/guidedTour.js
+++ client/components/guidedTour.js
@@ -1,57 +1,129 @@
 import React from 'react'
-import {Carousel, Container, Row, Col, Image} from 'react-bootstrap'
+import {
+  Carousel,
+  Container,
+  Row,
+  Col,
+  Image,
+  Button,
+  Modal
+} from 'react-bootstrap'
 
-const GuidedTour = () => {
-  return (
-    <div>
-      <Carousel>
-        <Carousel.Item className="carousel-item">
-          <img
-            className="d-block w-100"
-            src="https://images.unsplash.com/photo-1466978913421-dad2ebd01d17?ixlib=rb-1.2.1&auto=format&fit=crop&w=1267&q=80"
-            alt="Like cooking"
-          />
-          <Carousel.Caption className="about">
-            <h3> We love food. </h3>
-            <p>
-              We built a website to allow other foodies to discover new recipes.
-            </p>
-          </Carousel.Caption>
-        </Carousel.Item>
-        <Carousel.Item>
-          <img
-            className="d-block w-100"
-            src="https://images.unsplash.com/photo-1545601445-4d6a0a0565f0?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=1267&q=80"
-            alt="Stocked Pantry"
-          />
-          <Carousel.Caption className="about">
-            <h3>We have unused items in our pantry.</h3>
-            <p>
-              It's difficult figuring out what you can cook with the items you
-              already have. Meal.Match hopes to inspire you with recipes you can
-              cook without a trip to the grocery store.
-            </p>
-          </Carousel.Caption>
-        </Carousel.Item>
-        <Carousel.Item>
-          <img
-            className="d-block w-100"
-            src="https://images.unsplash.com/photo-1547860664-b8537ca5f833?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=1350&q=80"
-            alt="Women engineers"
-          />
-          <Carousel.Caption className="about">
-            <h3> We're talented coders. </h3>
-            <p>
-              Sometimes we just can't make it to the store and want to make
-              meals with the ingredients we already have. We couldn't find a
-              resource that offered us recipes catered towards our pantries, so
-              we built one!
-            </p>
-          </Carousel.Caption>
-        </Carousel.Item>
-      </Carousel>
-    </div>
-  )
+class GuidedTour extends React.Component {
+  constructor(props, context) {
+    super(props, context)
+
+    this.handleShow = this.handleShow.bind(this)
+    this.handleClose = this.handleClose.bind(this)
+
+    this.state = {
+      show: false
+    }
+  }
+
+  handleClose() {
+    this.setState({show: false})
+  }
+
+  handleShow() {
+    this.setState({show: true})
+  }
+
+  render() {
+    return (
+      <>
+        <Button
+          className="btn-responsive"
+          size="lg"
+          variant="primary"
+          onClick={this.handleShow}
+        >
+          Guided Tour
+        </Button>
+
+        <Modal show={this.state.show} onHide={this.handleClose} centered>
+          <Modal.Header closeButton>
+            <Modal.Title>Join us for a Tour of Meal.Match</Modal.Title>
+          </Modal.Header>
+          <Carousel>
+            <Carousel.Item className="carousel-item">
+              {/* <img
+                className="d-block w-100"
+                src="https://images.unsplash.com/photo-1466978913421-dad2ebd01d17?ixlib=rb-1.2.1&auto=format&fit=crop&w=1267&q=80"
+                alt="Like cooking"
+              /> */}
+              <Carousel.Caption className="about">
+                <h3>
+                  {' '}
+                  Take our quiz to receive suggestions of recipes that match the
+                  ingredients you have in your kitchen.
+                </h3>
+                <p>More text?</p>
+              </Carousel.Caption>
+            </Carousel.Item>
+            <Carousel.Item className="carousel-item">
+              <img
+                className="d-block w-100"
+                src="https://images.unsplash.com/photo-1545601445-4d6a0a0565f0?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=1267&q=80"
+                alt="Stocked Pantry"
+              />
+              <Carousel.Caption className="about">
+                <h3>
+                  Click on ingredients in the quiz to add them to your
+                  Ingredients Board. Change your mind about an item? You can
+                  remove it from the cutting board by clicking the X next to its
+                  name.{' '}
+                </h3>
+                <p>More text?</p>
+              </Carousel.Caption>
+            </Carousel.Item>
+            <Carousel.Item className="carousel-item">
+              <img
+                className="d-block w-100"
+                src="https://images.unsplash.com/photo-1547860664-b8537ca5f833?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=1350&q=80"
+                alt="Women engineers"
+              />
+              <Carousel.Caption className="about">
+                <h3>
+                  {' '}
+                  Any time you're ready to be matched with recipes, click the
+                  "Match Me" button.
+                </h3>
+                <p>
+                  Sometimes we just can't make it to the store and want to make
+                  meals with the ingredients we already have. We couldn't find a
+                  resource that offered us recipes catered towards our pantries,
+                  so we built one!
+                </p>
+              </Carousel.Caption>
+            </Carousel.Item>
+            <Carousel.Item className="carousel-item">
+              <img
+                className="d-block w-100"
+                src="https://images.unsplash.com/photo-1547860664-b8537ca5f833?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=1350&q=80"
+                alt="Women engineers"
+              />
+              <Carousel.Caption className="about">
+                <h3>
+                  Want to learn more? Visit our How It Works page! Ready to take
+                  the quiz?
+                </h3>
+                <p>Click on the buttons!</p>
+              </Carousel.Caption>
+            </Carousel.Item>
+          </Carousel>
+          <Modal.Footer>
+            <Button variant="secondary" onClick={this.handleClose}>
+              How It Works
+            </Button>
+            <Button variant="primary" onClick={this.handleClose}>
+              Take the Quiz
+            </Button>
+          </Modal.Footer>
+        </Modal>
+      </>
+    )
+  }
 }
 
 export default GuidedTour
--- client/components/home.js
+++ client/components/home.js
@@ -1,6 +1,7 @@
 import React from 'react'
 import {Link} from 'react-router-dom'
-import {Modal, Carousel, Row, Col, Button} from 'react-bootstrap'
+import {Row, Col, Button} from 'react-bootstrap'
+import {GuidedTour} from './index'
 
 export default class Home extends React.Component {
   constructor(props) {
@@ -42,56 +43,7 @@ export default class Home extends React.Component {
                   </Col>
                   <Col>
                     New to Meal.Match?
-                    <Button
-                      className="btn-responsive"
-                      size="lg"
-                      data-toggle="modal"
-                      data-target="#guidedTour"
-                    >
-                      Take the Guided Tour
-                    </Button>
-                    <Modal>
-                      <div class="modal" tabindex="-1" role="dialog">
-                        <div class="modal-dialog" role="document">
-                          <div class="modal-content">
-                            <div class="modal-header">
-                              <h5 class="modal-title">Modal title</h5>
-                              <button
-                                type="button"
-                                class="close"
-                                data-dismiss="modal"
-                                aria-label="Close"
-                              >
-                                <span aria-hidden="true">&times;</span>
-                              </button>
-                            </div>
-                            <div class="modal-body">
-                              <p>Modal body text goes here.</p>
-                            </div>
-                            <div class="modal-footer">
-                              <button type="button" class="btn btn-primary">
-                                Save changes
-                              </button>
-                              <button
-                                type="button"
-                                class="btn btn-secondary"
-                                data-dismiss="modal"
-                              >
-                                Close
-                              </button>
-                            </div>
-                          </div>
-                        </div>
-                      </div>
-                    </Modal>
-                    <div
-                      // class="modal fade"
-                      tabIndex="-1"
-                      role="dialog"
-                      aria-labelledby="ModalCarouselLabel"
-                      id="#guidedTour"
-                    />
-                    {/* <Modal id="#guidedTour">Hello!</Modal> */}
+                    <GuidedTour />
                   </Col>
                 </Row>
               </Col>
