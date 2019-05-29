bc45a3798e416f300fb5b11cc2a5de8265ff7617
KV Nguyen <kieuvyn12@gmail.com>
2019-05-28 23:35:39 -0400

    removing user seed data

--- script/seed.js
+++ script/seed.js
@@ -1,16 +1,11 @@
 'use strict'
 
 const db = require('../server/db')
-const {User, Question} = require('../server/db/models')
+const {Question} = require('../server/db/models')
 
 async function seed() {
   await db.sync({force: true})
   console.log('db synced!')
-
-  const users = await Promise.all([
-    User.create({email: 'cody@email.com', password: '123'}),
-    User.create({email: 'murphy@email.com', password: '123'})
-  ])
 
   const questions = await Promise.all([
     Question.create({
