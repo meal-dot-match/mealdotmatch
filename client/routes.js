cb42087c0c76f71de9882bb08d0d316bb14f6f4d
KV Nguyen <kieuvyn12@gmail.com>
2019-05-28 22:41:48 -0400

    removing routes and grocery bag

--- client/routes.js
+++ client/routes.js
@@ -1,15 +1,12 @@
 import React, {Component} from 'react'
 import {Route, Switch} from 'react-router-dom'
 import {
-  Login,
-  Signup,
   Home,
   Quiz,
   About,
   HowItWorks,
   SingleRecipe,
-  GroceryList,
-  GroceryBag
+  GroceryList
 } from './components/index'
 import Results from './components/results'
 import SearchBarResults from './components/searchbarResults'
@@ -24,14 +21,11 @@ class Routes extends Component {
         <Route exact path="/" component={Home} />
         <Route exact path="/quiz" component={Quiz} />
         <Route exact path="/about" component={About} />
-        <Route exact path="/login" component={Login} />
-        <Route exact path="/signup" component={Signup} />
         <Route exact path="/results" component={Results} />
         <Route exact path="/howitworks" component={HowItWorks} />
         <Route exact path="/searchbarresults" component={SearchBarResults} />
         <Route exact path="/grocerylist" component={GroceryList} />
         <Route path="/recipes" component={SingleRecipe} />
-        <Route path="/groceryBag" component={GroceryBag} />
       </Switch>
     )
   }
