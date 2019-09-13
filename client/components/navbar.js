2f493c8c41090c8cc57fef917185dda1c088c461
Ann Kettles <ann.a.kettles@gmail.com>
2019-09-12 21:12:31 -0400

    updated navbar to func component

--- client/components/navbar.js
+++ client/components/navbar.js
@@ -1,57 +1,47 @@
-import React from 'react'
+import React, {useState} from 'react'
 import {Link} from 'react-router-dom'
 import {Navbar, Nav, FormControl, Form, Button} from 'react-bootstrap'
 
-class Navigation extends React.Component {
-  constructor() {
-    super()
-    this.state = {
-      value: ''
-    }
-    this.handleChange = this.handleChange.bind(this)
-  }
+const Navigation = () => {
+  const [searchInput, setSearchInput] = useState('')
 
-  handleChange(event) {
-    this.setState({
-      value: event.target.value
-    })
+  const handleInput = ({target: {value}}) => {
+    setSearchInput(value)
   }
+  return (
+    <Navbar collapseOnSelect expand="lg" bg="light" variant="light">
+      <Navbar.Brand href="/">Meal.Match</Navbar.Brand>
+      <Navbar.Toggle aria-controls="responsive-navbar-nav" />
+      <Navbar.Collapse id="responsive-navbar-nav">
+        <Nav className="mr-auto">
+          <Nav.Link href="/howitworks">How It Works</Nav.Link>
+          <Nav.Link href="/about">Our Story</Nav.Link>
+          <Nav.Link href="/quiz">Start Now</Nav.Link>
+        </Nav>
 
-  render() {
-    return (
-      <Navbar collapseOnSelect expand="lg" bg="light" variant="light">
-        <Navbar.Brand href="/">Meal.Match</Navbar.Brand>
-        <Navbar.Toggle aria-controls="responsive-navbar-nav" />
-        <Navbar.Collapse id="responsive-navbar-nav">
-          <Nav className="mr-auto">
-            <Nav.Link href="/howitworks">How It Works</Nav.Link>
-            <Nav.Link href="/about">Our Story</Nav.Link>
-            <Nav.Link href="/quiz">Start Now</Nav.Link>
-          </Nav>
-
-          <Form inline>
-            <FormControl
-              type="text"
-              placeholder="Search recipes..."
-              className="mr-sm-2"
-              value={this.state.value}
-              onChange={this.handleChange}
-            />
-            <Link
-              to={{
-                pathname: '/searchbarresults',
-                searchBar: this.state.value.replace(/\s/g, '+')
-              }}
-            >
-              <Button variant="outline-info" type="submit">
-                Search
-              </Button>
-            </Link>
-          </Form>
-        </Navbar.Collapse>
-      </Navbar>
-    )
-  }
+        <Form inline>
+          <FormControl
+            type="text"
+            placeholder="Search recipes..."
+            className="mr-sm-2"
+            value={searchInput}
+            onChange={handleInput}
+          />
+          {console.log(searchInput)}
+          <Link
+            to={{
+              pathname: '/searchbarresults',
+              searchBar: searchInput.replace(/\s/g, '+')
+            }}
+          >
+            <Button variant="outline-info" type="submit">
+              Search
+            </Button>
+          </Link>
+        </Form>
+      </Navbar.Collapse>
+    </Navbar>
+  )
 }
 
 export default Navigation
